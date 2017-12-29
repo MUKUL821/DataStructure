f060a1a133092b2205d6bf6b73af7ddc55a9ff9a
Mukul Bajpai <mukul@celect.com>
2017-12-29 11:32:27 -0800

    add design questions

---
+++ src/TODO.java
@@ -0,0 +1,88 @@
+
+public class TODO {
+//200. Number of Islands
+	//130. Surrounded Regions
+	//170. Two Sum III - Data structure design
+	//159. Longest Substring with At Most Two Distinct Characters
+	//Longest consecutive sequence in Binary tree
+	//longest-substring-with-at-most-two-distinct-characters
+	//paint-fence
+	//252. Meeting Rooms
+	/**
+	 * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+	For example,
+	Given [[0, 30],[5, 10],[15, 20]],
+	return false.
+	 * Given a time represented in the format "HH:MM", form the next closest time by reusing the current digits. There is no limit on how many times a digit can be reused.
+
+You may assume the given input string is always valid. For example, "01:34", "12:09" are all valid. "1:34", "12:9" are all invalid.
+
+Example 1:
+
+Input: "19:34"
+Output: "19:39"
+Explanation: The next closest time choosing from digits 1, 9, 3, 4, is 19:39, which occurs 5 minutes later.  It is not 19:33, because this occurs 23 hours and 59 minutes later.
+Example 2:
+
+Input: "23:59"
+Output: "22:22"
+Explanation: The next closest time choosing from digits 2, 3, 5, 9, is 22:22. It may be assumed that the returned time is next day's time since it is smaller than the input time numerically.
+
+	 * **/
+	
+	/**
+	 * 683. K Empty Slots
+DescriptionHintsSubmissionsDiscussSolution
+DiscussPick One
+There is a garden with N slots. In each slot, there is a flower. The N flowers will bloom one by one in N days. In each day, there will be exactly one flower blooming and it will be in the status of blooming since then.
+
+Given an array flowers consists of number from 1 to N. Each number in the array represents the place where the flower will open in that day.
+
+For example, flowers[i] = x means that the unique flower that blooms at day i will be at position x, where i and x will be in the range from 1 to N.
+
+Also given an integer k, you need to output in which day there exists two flowers in the status of blooming, and also the number of flowers between them is k and these flowers are not blooming.
+
+If there isn't such day, output -1.
+
+Example 1:
+Input: 
+flowers: [1,3,2]
+k: 1
+Output: 2
+Explanation: In the second day, the first and the third flower have become blooming.
+Example 2:
+Input: 
+flowers: [1,2,3]
+k: 1
+Output: -1
+Note:
+The given array will be in the range [1, 20000].
+
+	 * 
+	 * **/
+	
+	
+	
+	/**
+	 * 296. Best Meeting Point
+DescriptionHintsSubmissionsDiscussSolution
+DiscussPick One
+A group of two or more people wants to meet and minimize the total travel distance. You are given a 2D grid of values 0 or 1, where each 1 marks the home of someone in the group. The distance is calculated using Manhattan Distance, where distance(p1, p2) = |p2.x - p1.x| + |p2.y - p1.y|.
+
+For example, given three people living at (0,0), (0,4), and (2,2):
+
+1 - 0 - 0 - 0 - 1
+|   |   |   |   |
+0 - 0 - 0 - 0 - 0
+|   |   |   |   |
+0 - 0 - 1 - 0 - 0
+The point (0,2) is an ideal meeting point, as the total travel distance of 2+2+2=6 is minimal. So return 6.
+
+
+	 * 
+	 * **/
+	//next-closest-time
+
+		
+}
